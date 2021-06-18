b8d0d882caa990fdae7ea0b7d3a612baace12242
vlimadev <vinilimadev@gmail.com>
2021-06-18 15:56:59 -0300

    commit 5

--- js/script.js
+++ js/script.js
@@ -1,15 +1,20 @@
-function leiamais(){
+function leiaMais(){
     var pontos=document.getElementById("pontos");
     var maisTexto=document.getElementById("mais");
     var btnleiamais=document.getElementById("btnleiamais");
+    var txtresumo=document.getElementsByClassName("txt-resumo")
+    
 
         if(pontos.style.display ==="none"){
             pontos.style.display="inline";
             maisTexto.style.display="none";
+            
             btnleiamais.innerHTML="Ver mais";
         }else{
             pontos.style.display="none";
             maisTexto.style.display="inline";
+            btnleiamais.style.width="80px"
             btnleiamais.innerHTML="Ver menos";
+           txtresumo.classname="txt-resumo-um";
         }
 }
