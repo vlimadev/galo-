286d16a278b61a3b02f8989ca5aa6e1229b03f95
vlimadev <vinilimadev@gmail.com>
2021-06-18 22:06:49 -0300

    commit 7

--- js/script.js
+++ js/script.js
@@ -18,3 +18,12 @@ function leiaMais(){
            txtresumo.classname="txt-resumo-um";
         }
 }
+
+function mostrar(elemento){
+    var display = document.getElementById(elemento).style.display;
+      if(display == "none"){
+          document.getElementById(elemento).style.display = 'block';
+      }else{
+          document.getElementById(elemento).style.display = 'none';
+      }    
+  }
